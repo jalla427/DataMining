4880bc47d939422212c63eccb8967c8dba13b77b
Jack Allaire <jalla427@uwsp.edu>
2018-09-17 11:27:08 -0500

    Adding Lab 1

---
+++ lab1.php
@@ -0,0 +1,12 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php print "lab1"; ?></title>
+    <script src="hello.js"><script>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+ <?php print "Hello World"; ?>
+</body>
+</html>
